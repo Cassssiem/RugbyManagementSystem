5a49d0892a5fd85047e504f1a13150520b15c878
Cassiem <cassssiem@gmail.com>
2026-08-07 08:22:22 +0200

    Delete New folder/Front-end directory

--- New folder/Front-end/src/api/sponsors.js
+++
@@ -1,8 +0,0 @@
-import { apiClient } from "./client";
-
-export const sponsorsApi = {
-  submit: (payload) => apiClient.post("/SponsorInquiry", payload),
-  getAll: () => apiClient.get("/SponsorInquiry"),
-  markReviewed: (id) => apiClient.put(`/SponsorInquiry/${id}/reviewed`, {}),
-  delete: (id) => apiClient.delete(`/SponsorInquiry/${id}`),
-};
